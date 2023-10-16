21bbad40a9544bf61262c13bca61854f849704b7
MatthewDent03 <N00220082@iadt.ie>
2023-10-16 14:41:15 +0100

    created the page controller to be used as a route

---
+++ database/seeders/DoctorsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Doctor;
+
+class DoctorsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        Doctor::factory(10)->create();
+    }
+}
