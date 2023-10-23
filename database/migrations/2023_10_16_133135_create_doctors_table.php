55edefe4df9a8a818fc251a157289c5b6804a8f6
MatthewDent03 <N00220082@iadt.ie>
2023-10-23 08:33:14 +0100

    Used rollback function to seed data and refresh

--- database/migrations/2023_10_16_133135_create_doctors_table.php
+++ database/migrations/2023_10_16_133135_create_doctors_table.php
@@ -3,16 +3,22 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Doctor;
 
 return new class extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('facility');
+            $table->string('email');
+            $table->string('phone_number');
             $table->timestamps();
         });
     }
@@ -20,7 +26,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('doctors');
     }
