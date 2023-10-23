d570a00b24eb58da854627591659fe63d2eb6c88
MatthewDent03 <N00220082@iadt.ie>
2023-10-23 08:31:35 +0100

    Used the migrate:rollback method to fix seed data.

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+    
 }
--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'facility',
+        'email',
+        'phone_number',
+    ];
 }
