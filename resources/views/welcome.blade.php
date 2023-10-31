a85b3687c113a7e15154127711294045ec2925e6
MatthewDent03 <N00220082@iadt.ie>
2023-10-31 11:18:42 +0000

    Inputted routes and controller in web.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Anne's Book Store</title>
+        <title>Matt's Doctor Page</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
