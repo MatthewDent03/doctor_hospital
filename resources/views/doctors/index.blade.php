fb4f24142af9ded577f86eca4db6d22021095609
MatthewDent03 <N00220082@iadt.ie>
2023-10-31 11:47:33 +0000

    introduced gist to index view to interact with nav

--- resources/views/doctors/index.blade.php
+++ resources/views/doctors/index.blade.php
@@ -1,34 +1,30 @@
-@extends('layouts.app')
-@section('content')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('All Doctors') }}
+        </h2>
+    </x-slot>
 
-<div class="container">
-    <h1>ALL Doctors</h1>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone Number</th>
-                <th>Image</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($doctors as $doctor)
-            <tr>
-                <td><a href="{{ route('doctors.show', $doctor) }}">{{ $doctor->first_name }}</a></td>
-                <td>{{ $doctor->email }}</td>
-                <td>{{ $doctor->phone_number }}</td>
-                <td>
-                    @if ($doctor->doctor_image)
-                        <img src="{{ $doctor->doctor_image }}"
-                        alt="{{ $doctor->title }}" width="100">
-                    @else
-                        No Image
-                    @endif
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
-@endsection
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            <a href="{{ route('doctors.create') }}" class="btn-link btn-lg mb-2">Add a Doctor</a>
+            @forelse ($doctors as $doctor)
+                <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                    <h2 class="font-bold text-2xl">
+                    <a href="{{ route('doctors.show', $doctor) }}">{{ $doctor->first_name }}{{ $doctor->last_name }}</a>
+                    </h2>
+                    <p class="mt-2">
+                        {{ $doctor->email }}
+                        {{$doctor->phone_number}}
+                        {{$doctor->facility}}
+                    </p>
+
+                </div>
+            @empty
+            <p>No doctors</p>
+            @endforelse
+
+        </div>
+    </div>
+</x-app-layout>
