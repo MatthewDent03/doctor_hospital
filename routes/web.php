c059eeeb0b58d142a7d868183e4cc80b8a0d8529
MatthewDent03 <N00220082@iadt.ie>
2023-10-31 10:56:02 +0000

    Json filr error ocurred trying to instal npm

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\DoctorController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +14,18 @@ use App\Http\Controllers\DoctorController;
 |
 */
 
-Route::resource('/doctors',DoctorController::class);
-// Route::get('/doctors/{doctor}',[DoctorController::class,'show'])->name('doctors.show');
-// Route::get('/doctors/{doctor}',[DoctorController::class,'create'])->name('doctors.create');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
