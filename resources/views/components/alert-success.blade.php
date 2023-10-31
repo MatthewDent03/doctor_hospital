971d905b8714ed716d1b49b56f85596b9c0ecec0
MatthewDent03 <N00220082@iadt.ie>
2023-10-31 13:13:44 +0000

    Created view edit, added form data for editing old

--- resources/views/components/alert-success.blade.php
+++ resources/views/components/alert-success.blade.php
@@ -3,4 +3,5 @@
     <div class="mb-4 px-4 py-2 bg-green-100 border border-green-200 text-green-700 rounded-md">
         {{ $slot }}
     </div>
+    @endif
 </div>
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,11 +1,11 @@
+<!-- resources/views/components/textarea.blade.php -->
 <div>
     @props(['disabled' => false, 'field' => '', 'value' => ''])
 
-    <input 
-        {{ $disabled ? 'disabled' : '' }} 
+    <textarea
+        {{ $disabled ? 'disabled' : '' }}
         {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}
-        value="{{ $value }}"
-    >
+    >{{ $value }}</textarea>
 
     @error($field)
         <div class="text-red-600 text-sm">{{ $message }}</div>
