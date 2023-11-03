b4b71291d62fae6bba1ca03a72daba75b3f5e45b
MatthewDent03 <N00220082@iadt.ie>
2023-11-03 10:44:40 +0000

    Index for each error occurred trying to add button

--- resources/views/doctors/index.blade.php
+++ resources/views/doctors/index.blade.php
@@ -7,8 +7,6 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <button type="submit" class="btn btn-primary">Filter by Ascending</button>
-            </form>
             <a href="{{ route('doctors.filterAscending') }}" class="btn btn-primary">Filter by Ascending</a>
             <a href="{{ route('doctors.filterDescending') }}" class="btn btn-primary">Filter by Descending</a>
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
