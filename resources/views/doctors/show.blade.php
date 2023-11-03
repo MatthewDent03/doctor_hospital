af5779b7c44b39ef5b9d9f5a46e37c4413f1076e
MatthewDent03 <N00220082@iadt.ie>
2023-11-03 10:39:56 +0000

    Created a route, button and function for filter

--- resources/views/doctors/show.blade.php
+++ resources/views/doctors/show.blade.php
@@ -11,6 +11,7 @@
             <x-alert-success>
                 {{ session('success') }}
             </x-alert-success>
+            
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                     <table class="table table-hover">
