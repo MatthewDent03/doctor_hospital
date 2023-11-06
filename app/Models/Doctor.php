3d10989d50f058136fd267ab49db2f7a88cbdedf
MatthewDent03 <N00220082@iadt.ie>
2023-11-06 12:29:16 +0000

    Commented the doctor php

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -1,5 +1,6 @@
 <?php
-
+//This file displays the data of the database. It is generally used as a class in other files
+//This will work with object orientated syntax to interact with the database
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,7 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     use HasFactory;
-
+//The protected function sets all the attributes together for the create and update functions in the controller
     protected $guarded = [];
 
     // protected $fillable = [
