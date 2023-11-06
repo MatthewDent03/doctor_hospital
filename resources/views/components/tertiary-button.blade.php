48412a36aaca544f85e5706d2f7f49e1028779cc
MatthewDent03 <N00220082@iadt.ie>
2023-11-06 12:37:45 +0000

    commented tertiary button

--- resources/views/components/tertiary-button.blade.php
+++ resources/views/components/tertiary-button.blade.php
@@ -1,3 +1,4 @@
 <button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-red-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-400 focus:bg-red-400 active:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
+<!-- created a third button for red colour used for delete button in show/edit views -->
