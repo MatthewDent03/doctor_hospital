6a33dfd72ec721f0fb7a742ddff4939b05c4b573
MatthewDent03 <N00220082@iadt.ie>
2023-11-06 12:40:31 +0000

    commented the create blade

--- resources/views/doctors/create.blade.php
+++ resources/views/doctors/create.blade.php
@@ -1,10 +1,10 @@
-<x-app-layout>
+<x-app-layout> <!-- Implementing an app layout from the layouts folder and calling the app.blade.php -->
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Create Doctor') }}
         </h2>
     </x-slot>
-
+<!-- Creating a button for the store function in the controller to be active -->
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
@@ -27,7 +27,7 @@
                         class="w-full mt-6"
                         :value="@old('last_name')"></x-text-input>
 
-                    <!-- I created a new component called textarea, you will need to do the same to using the x-textarea component -->
+                    <!-- Created text inputs and text areas to input data into the variables for the components -->
                     <x-textarea
                         name="facility"
                         field="facility"
@@ -52,7 +52,7 @@
                         field="phone_number"
                         :value="@old('phone_number')">>
                     </x-text-input>
-
+<!-- Creating a button for the create function -->
                     <x-primary-button class="mt-6">Create Doctor</x-primary-button>
                 </form>
             </div>
