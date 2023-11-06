b9c57f9211705153928da492c9cc54e2bbd0258a
MatthewDent03 <N00220082@iadt.ie>
2023-11-06 12:43:23 +0000

    commented edit blade

--- resources/views/doctors/edit.blade.php
+++ resources/views/doctors/edit.blade.php
@@ -1,10 +1,10 @@
-<x-app-layout>
+<x-app-layout> <!-- Inputting layout from app blade for edit page -->
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Edit Doctor') }}
         </h2>
     </x-slot>
-
+<!-- Created a route through a button to the update function in the controller as well as text inputs and areas for data with old values as temp data to be edited -->
     <div class="py-12">
         <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
@@ -56,7 +56,7 @@
                     class="w-full"
                     :value="@old('phone_number', $doctor->phone_number)">
                 </x-text-input>
-                <x-primary-button class="mt-6">Save Edit</x-primary-button>
+                <x-primary-button class="mt-6">Save Edit</x-primary-button> <!-- Created a save button to route to store function -->
                 </form>
             </div>
         </div>
