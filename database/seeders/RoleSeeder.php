e285dd8842bbe07eb780b050c7441ccc1f9dc9a4
MatthewDent03 <N00220082@iadt.ie>
2023-11-10 09:52:36 +0000

    edited role seeder to have admin and user

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -12,6 +12,14 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $role_admin = new Role();
+        $role_admin->name = 'admin';
+        $role_admin->description = 'An Administrator user';
+        $role_admin->save();
+
+        $role_user = new Role();
+        $role_user->name = 'user';
+        $role_user->description = 'An ordinary user';
+        $role_user->save();
     }
 }
