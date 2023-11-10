4a6b0adfdea066522337891a82296df200db19a9
MatthewDent03 <N00220082@iadt.ie>
2023-11-10 09:57:12 +0000

    edited user seeder to fill in admin user info

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,6 +12,23 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $role_admin = Role::where('name', 'admin')->first();
+        $role_user = Role::where('name', 'user')->first();
+
+        $admin = new User();
+        $admin->name = 'Matt Dent';
+        $admin->email = 'N00220082@iadt.ie';
+        $admin->password = Hash::make('password');
+        $admin->save();
+
+        $admin->roles()->attach($role_admin);
+
+        $user = new User();
+        $user->name = 'Bianca Sloane';
+        $user->email = 'bianca@gmail.com';
+        $user->password = Hash::make('password');
+        $user->save();
+
+        $user->roles()->attach($role_user);
     }
 }
