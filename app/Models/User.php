bc8e45c1fe93e17b773abc24c7d07ce111f2be5a
MatthewDent03 <N00220082@iadt.ie>
2023-11-10 09:49:03 +0000

    added roles, authorisation functions to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,32 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany('App\Models\Role', 'user_role');
+    }
+
+    public function authorizeRoles($roles)
+    {
+        if(is_array($roles)){
+            return $this->hasAnyRole($roles) ||
+            abort (401, 'This user does not have access to this function');
+        }
+        return $this->hasRole($roles) ||
+        abort(401, 'This user does not have access to this function');
+    }
+
+
+    public function hasRole($role)
+    {
+        return null !== $this->roles()->where('name', $role)->first();
+    }
+
+    // Remember, a user can have many roles. This function checks the user 
+    // against all their roles
+    public function hasAnyRole($roles)
+    {
+        return null !== $this->roles()->whereIn('name', $roles)->first();
+    }
 }
