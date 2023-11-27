d4fb1f5cf41ab3f64b204cb26fc7df8ddd581145
MatthewDent03 <N00220082@iadt.ie>
2023-11-27 12:20:29 +0000

    added and edited an index php file for the hospitals to display

---
+++ resources/views/admin/hospitals/index.blade.php
@@ -0,0 +1,44 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('All Hospitals') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+         
+            <x-alert-success>
+                {{ session('success') }}
+            </x-alert-success>
+        
+            <x-primary-button>
+                <a href="{{ route('admin.hospitals.create') }}">Add a Hospital</a>
+            </x-primary-button>
+
+            @forelse ($hospitals as $hospital)
+                <x-card>
+                  
+                        <a href="{{ route('admin.hospitals.show', $hospital) }}" class="font-bold text-2xl">{{ $hospital->name }}</a>
+            
+                        <p class="mt-2 text-gray-700">
+                            <span class="font-bold">ID:</span> {{ $hospital->id }}
+                        </p>
+                        <p class="mt-2 text-gray-700">
+                            <span class="font-bold">Name:</span> {{ $hospital->name }}
+                        </p>
+                        <p class="mt-2 text-gray-700">
+                            <span class="font-bold">Address:</span> {{ $hospital->address }}
+                        </p>
+                        <p class="mt-2 text-gray-700">
+                            <span class="font-bold">Phone Number:</span> {{ $hospital->phone_number }}
+                        </p>
+            
+                </x-card>   
+            @empty
+                <p>No Hospitals</p>
+            @endforelse
+            
+        </div>
+    </div>
+</x-app-layout>
