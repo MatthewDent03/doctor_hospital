fba3735982fe9f224be0e6cd2c5f19e22fab67b5
MatthewDent03 <N00220082@iadt.ie>
2023-12-04 14:16:41 +0000

    navigation and app files are erroring with the $slot function for design with components

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,7 +2,7 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
+        <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
@@ -97,7 +97,7 @@
             </div>
         </div>
     </div>
-
+        
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
