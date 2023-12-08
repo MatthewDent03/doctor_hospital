11d8119be25154503d3d6b4f9671c42e7f4a41e6
MatthewDent03 <N00220082@iadt.ie>
2023-12-08 10:12:35 +0000

    altered seeder to apply random id integer

--- database/seeders/PatientSeeder.php
+++ database/seeders/PatientSeeder.php
@@ -9,9 +9,19 @@ class PatientSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * 
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Patient::factory()
+        ->times(3)
+        ->create();
+
+        foreach(Doctor::all() as $doctor)
+        {
+            $patients = Patient::inRandomOrder()->take(rand(1,3))->pluck('id');
+            $doctor->patients()->attach($patients);
+        }
     }
 }
