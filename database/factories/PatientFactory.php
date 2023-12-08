fa8e0cdf1366ce870d54d69a9d4e2fd485452607
MatthewDent03 <N00220082@iadt.ie>
2023-12-08 10:09:56 +0000

    added faker data to the many to many factory

--- database/factories/PatientFactory.php
+++ database/factories/PatientFactory.php
@@ -17,7 +17,13 @@ class PatientFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'emergency_contact' => $this->faker->name,
+            'phone_number' => $this->faker->realText,
+            'emergency_number' => $this->faker->realText,
+            'address' => $this->faker->address,
+            'age' => $this->faker->numberBetween(18, 70),
+            'gender' => $this->faker->realText
         ];
     }
 }
